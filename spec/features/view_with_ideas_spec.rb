31847f1bcd5b64d00e74cb9a85b06e048f5c1d25
Christopher Soden <karavol27@gmail.com>
2016-08-15 18:55:44 -0600

    Add created_at field to table. Create idea factory, create test

---
+++ spec/features/view_with_ideas_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "View with Ideas" do
+  scenario 'view a page with 3 ideas' do
+    ideas = [create(:idea)]
+    sleep 1
+    ideas << create(:idea)
+    sleep 1
+    ideas << create(:idea)
+
+    byebug
+    ideas.each do |idea|
+      expect(page).to have_content(idea.title)
+      expect(page).to have_content(idea.body)
+    end
+  end
+end
