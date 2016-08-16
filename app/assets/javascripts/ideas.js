66d1c31d52ba34b3e740ea973a457b14a0181d67
Christopher Soden <karavol27@gmail.com>
2016-08-16 15:26:09 -0600

    [WIP] javascript is responding to button click and getting data from text field

---
+++ app/assets/javascripts/ideas.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+  $(":button[name=submit]").on('click', function(){
+    console.log("button was clicked");
+    var title = $('#title').val();
+    var body = $('#body').val();
+    debugger;
+    // $.ajax({
+    //   method: "POST",
+    //
+    // })
+  })
+
+});
