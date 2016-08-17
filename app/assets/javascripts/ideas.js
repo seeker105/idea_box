2e6b778d78a20726aa31203b49252d72319d4fd1
Christopher Soden <karavol27@gmail.com>
2016-08-17 16:23:17 -0600

    Add delete button to each idea

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   fetchIdeas();
 
   $(":button[name=submit]").on('click', submitIdea);
-  $(":button[name=fetch]").on('click', fetchIdeas);
+  $(":button[name=delete]").on('click', deleteIdea);
 });
 
 
@@ -41,6 +41,6 @@ function renderIdeas(ideaData){
   ideasDiv = $("#ideasDiv");
   ideasDiv.html(""),
   ideaData.forEach(function(idea){
-    ideasDiv.append('<div class="row">Title: ' + idea.title + ' Body: ' + idea.body + ' Quality: ' + idea.quality + '</div><br />');
+    ideasDiv.append('<div class="row idea-' + idea.id + '">' + '<input type="button" name="delete" id="' + idea.id + '"value="Delete" class="btn btn-default pull-right">'+ 'Title: ' + idea.title + ' Body: ' + idea.body + ' Quality: ' + idea.quality + '</div><br />');
   });
 };
