0d0b2dd52109f4156246ce62bdf9f211c363843e
Christopher Soden <karavol27@gmail.com>
2016-08-17 18:45:56 -0600

    [WIP] functions are bound to the thumbs up/down buttons

--- public/assets/ideas-734c9c1820b4ea52cb8b3fffe8d7445b2c62da3e4ad69f4c2ac6bec54e51d385.js
+++ public/assets/ideas-734c9c1820b4ea52cb8b3fffe8d7445b2c62da3e4ad69f4c2ac6bec54e51d385.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   $(":button[name=submit]").on('click', submitIdea);
   $("#ideasDiv").on('click', ".delete-button", startDelete);
-  $("#ideasDiv").on('click', ".thumbs-up-button", incrementQuality);
+  $("#ideasDiv").on('click', ".thumbs-up-button", increase);
   $("#ideasDiv").on('click', ".thumbs-down-button", decrementQuality);
 });
 
@@ -45,8 +45,8 @@ function renderIdeas(ideaData){
   ideaData.forEach(function(idea){
     ideasDiv.append('<div class="row" id="' + idea.id + '">' + '\
 <input type="button" name="delete" value="Delete" class="btn btn-default pull-right delete-button"> \
-<input type="button" name="thumbs-up-button" value="Thumbs Up" class="btn btn-default pull-right thumbs-up-button"> \
-<input type="button" name="thumbs-down-button" value="Thumbs Down" class="btn btn-default pull-right thumbs-down-button"> \
+<input type="button" name="thumbs-up-button" value="Thumbs Up" class="btn btn-default pull-right delete-button"> \
+<input type="button" name="thumbs-down-button" value="Thumbs Down" class="btn btn-default pull-right delete-button"> \
 Title: ' + idea.title + ' Body: ' + idea.body + ' Quality: ' + idea.quality + '</div><br />');
   });
 };
@@ -61,8 +61,7 @@ function startDelete(){
 };
 
 function incrementQuality(){
-};
-
-function decrementQuality(){
   debugger;
 };
+
+function decrementQuality(){};
